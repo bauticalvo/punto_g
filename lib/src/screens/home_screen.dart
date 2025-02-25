ab7372beb26d035dd170c4c9fb1364fc5523b05a
bauticalvo <bito.bc@gmail.com>
2025-02-25 15:34:03 -0300

    second commit

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int count = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromRGBO(68, 94, 96, 0.5),
-      body: Center(child: Text("home screen")),
+      body: Center(child: Text('Click${ count == 1 ? '' : 's'}: $count', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xFFFE9E02),
+          child: Icon(Icons.add),
+          onPressed: () {
+            count++;
+            setState((){});
+          },
+        ),
       );
   }
 }
